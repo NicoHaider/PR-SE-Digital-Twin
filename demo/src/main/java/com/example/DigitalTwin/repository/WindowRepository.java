eb66f04c6739fce7d6c56afd2f19ee82df51f8d2
NicoHaider <163557279+NicoHaider@users.noreply.github.com>
2024-04-17 09:58:50 +0200

    Delete demo directory

--- demo/src/main/java/com/example/DigitalTwin/repository/WindowRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.DigitalTwin.repository;
-
-import com.example.DigitalTwin.model.components.Window;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WindowRepository extends JpaRepository<Window, Long> {
-    // Hier könnten spezifische Methoden definiert werden, falls nötig.
-}
