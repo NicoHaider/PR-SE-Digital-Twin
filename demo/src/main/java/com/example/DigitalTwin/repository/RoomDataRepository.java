f137fa75cf5e75cba58cb99d318a222a78d46201
stefanbaj <105297079+stefanbaj@users.noreply.github.com>
2024-04-28 15:04:16 +0200

    test

--- demo/src/main/java/com/example/DigitalTwin/repository/RoomDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.DigitalTwin.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.DigitalTwin.model.RoomData;
-
-public interface RoomDataRepository extends JpaRepository<RoomData, Long> {
-
-}
