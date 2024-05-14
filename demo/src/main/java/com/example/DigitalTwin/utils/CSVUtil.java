7207002f13897c2422857eb4ee3f825ba3662f62
MaximilianPausch <maximilian.pausch@gmx.at>
2024-05-14 18:15:33 +0200

    f

---
+++ demo/src/main/java/com/example/DigitalTwin/utils/CSVUtil.java
@@ -0,0 +1,27 @@
+package com.example.DigitalTwin.utils;
+
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import com.example.DigitalTwin.model.Room;
+
+public class CSVUtil {
+
+    public static ByteArrayInputStream roomsToCSV(List<Room> rooms) {
+        StringBuilder csvContent = new StringBuilder();
+        csvContent.append("Room_ID,Name,Size,Doors,Windows,Lights,Fans\n");
+
+        for (Room room : rooms) {
+            csvContent.append(room.getId()).append(",")
+                    .append(room.getName()).append(",")
+                    .append(room.getSize()).append(",")
+                    .append(room.getDoors()).append(",")
+                    .append(room.getWindows()).append(",")
+                    .append(room.getLights()).append(",")
+                    .append(room.getFans()).append("\n");
+        }
+
+        return new ByteArrayInputStream(csvContent.toString().getBytes(StandardCharsets.UTF_8));
+    }
+}
