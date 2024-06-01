5f283608c1ba682d57399e7deaa26407ea591d4c
NicoHaider <163557279+NicoHaider@users.noreply.github.com>
2024-06-01 14:14:19 +0200

    Frontend changes

---
+++ Digitial-Twin-Frontend/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { RoomItemComponent } from './room/room-item/room-item.component';
+import { RoomListComponent } from './room/room-list/room-list.component';
+import { RoomService } from './room/room.service';
+import { RoomAddComponent } from './room/room-add&Edit/room-add.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app.routing.module';
+import { RoomDetailsComponent } from './room/room-details/room-details.component';
+import { DeviceItemComponent } from './device/device-item/device-item.component';
+import { DeviceService } from './device/device.service';
+import { DeviceEditComponent } from './device/device-Add&Edit/device-edit.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AddDeviceComponent } from './device/add-device/add-device.component';
+import { UpdateRoomComponent } from './room/update-room/update-room.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RoomItemComponent,
+    RoomListComponent,
+    RoomAddComponent,
+    RoomDetailsComponent,
+    DeviceItemComponent,
+    DeviceEditComponent,
+    AddDeviceComponent,
+    UpdateRoomComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [RoomService, DeviceService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Digitial-Twin-Frontend/src/app/device/device-item/device-item.component.ts
@@ -0,0 +1,43 @@
+import { Component, Input } from '@angular/core';
+import { Device, DeviceType } from '../device.model';
+import { DeviceService} from '../device.service';
+import { Router } from '@angular/router';
+import { Room } from '../../room/room.model';
+import { RoomService } from '../../room/room.service';
+
+@Component({
+  selector: 'app-device-item',
+  templateUrl: './device-item.component.html',
+  styleUrl: './device-item.component.css'
+})
+export class DeviceItemComponent {
+  @Input() roomIndex: number;
+  @Input() device: Device;
+  @Input() index: number = 0;
+
+  constructor(private deviceService: DeviceService, private roomService : RoomService, private router: Router) { 
+  }
+
+  getDeviceImagePath(deviceType: DeviceType): string {
+    return this.deviceService.getDeviceImagePath(deviceType);
+  } 
+
+  changeStatus(){
+     this.deviceService.updateDeviceStatus(this.device.id).subscribe(res=>{
+                this.device = res;
+              }, error=>{
+                console.log(error)
+              });
+  }
+
+  onEdit() {
+    this.deviceService.editModeChanged(true);
+    this.router.navigate(['/deviceEdit/'+ this.roomIndex + '/' + this.index]);
+  }
+
+  onDelete() {
+    //TODO: add a confirmation dialog
+    console.warn('are you sure you want to delete this device?');
+    this.roomService.deleteDevice(this.roomIndex, this.index);
+  }
+}
