0380703914c3ccb5c1e505daad9aa4e70c3e0982
MaximilianPausch <maximilian.pausch@gmx.at>
2024-06-02 12:46:44 +0200

    fixing room csv structure

--- demo/src/main/java/com/example/DigitalTwin/utils/CSVUtil.java
+++ demo/src/main/java/com/example/DigitalTwin/utils/CSVUtil.java
@@ -11,18 +11,18 @@ public class CSVUtil {
 
     public static ByteArrayInputStream roomsToCSV(List<Room> rooms) {
         StringBuilder csvContent = new StringBuilder();
-        csvContent.append("Room_ID,Name,Type,Size,Device_Count,Climate_Data_Description\n");
+        csvContent.append("Room_ID;Name;Type;Size;Device_Count;Climate_Data_Description\n");
 
         for (Room room : rooms) {
             String climateDataDescription = room.getRoomDataList().stream()
                     .map(data -> data.toString())  // Assuming RoomData has a meaningful toString implementation
                     .collect(Collectors.joining("; "));
 
-            csvContent.append(room.getId()).append(",")
-                    .append(room.getName()).append(",")
-                    .append(room.getType()).append(",")
-                    .append(room.getSize()).append(",")
-                    .append(room.getDevices().size()).append(",")
+            csvContent.append(room.getId()).append(";")
+                    .append(room.getName()).append(";")
+                    .append(room.getType()).append(";")
+                    .append(room.getSize()).append(";")
+                    .append(room.getDevices().size()).append(";")
                     .append(climateDataDescription).append("\n");
         }
 
