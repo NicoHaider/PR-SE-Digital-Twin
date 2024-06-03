17cb6d7d979befaeaf1b164a75a5364d51484994
lisawasdf <164348001+lisawasdf@users.noreply.github.com>
2024-06-03 20:20:20 +0200

    Update RoomDataController.java

--- demo/src/main/java/com/example/DigitalTwin/controller/RoomDataController.java
+++ demo/src/main/java/com/example/DigitalTwin/controller/RoomDataController.java
@@ -1,14 +1,14 @@
 package com.example.DigitalTwin.controller;
 
-import com.example.DigitalTwin.model.RoomData;
-import com.example.DigitalTwin.service.RoomDataService;
-import com.example.DigitalTwin.utils.ScheduledTasks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.DigitalTwin.model.RoomData;
+import com.example.DigitalTwin.service.RoomDataService;
+import com.example.DigitalTwin.utils.ScheduledTasks;
 import java.util.List;
 
 @RestController
@@ -18,7 +18,7 @@ public class RoomDataController {
     @Autowired
     private RoomDataService roomDataService;
 
-    @GetMapping("/{id}")
+    @GetMapping("getBy/{id}")
     public List<RoomData> getRoomDataByRoom(@PathVariable long id) {
         return roomDataService.getRoomDataByRoom(id);
     }
