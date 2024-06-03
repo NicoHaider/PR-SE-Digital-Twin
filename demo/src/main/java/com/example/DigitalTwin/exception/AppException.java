92d791089ea700274c266eb06f50cac5d48f60a2
stefanbaj <105297079+stefanbaj@users.noreply.github.com>
2024-06-03 23:00:11 +0200

    Delete demo directory

--- demo/src/main/java/com/example/DigitalTwin/exception/AppException.java
+++
@@ -1,9 +0,0 @@
-package com.example.DigitalTwin.exception;
-
-public class AppException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-
-	public AppException(String message) {
-		super(message);
-	}
-}
