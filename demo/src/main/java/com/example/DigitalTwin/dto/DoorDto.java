607a47fc2d975b9f081393cfc3cf94ac582954a8
stefanbaj <105297079+stefanbaj@users.noreply.github.com>
2024-06-06 00:16:56 +0200

    setup troubles

--- demo/src/main/java/com/example/DigitalTwin/dto/DoorDto.java
+++ demo/src/main/java/com/example/DigitalTwin/dto/DoorDto.java
@@ -1,7 +1,5 @@
 package com.example.DigitalTwin.dto;
 
-//import org.junit.jupiter.api.Test;
-//import static org.junit.jupiter.api.Assertions.*;
 public class DoorDto {
 
     private int doorID;
@@ -24,31 +22,4 @@ public class DoorDto {
         this.name = name;
     }
 
-/*
-    @Test
-    public void testGettersAndSetters() {
-        DoorDto doorDto = new DoorDto();
-
-        doorDto.setDoorID(123);
-        doorDto.setName("Main Entrance");
-
-        assertEquals(123, doorDto.getDoorID());
-        assertEquals("Main Entrance", doorDto.getName());
-    }
-
-    @Test
-    public void testDoorID() {
-        DoorDto doorDto = new DoorDto();
-
-        doorDto.setDoorID(456);
-        assertEquals(456, doorDto.getDoorID());
-    }
-
-    @Test
-    public void testName() {
-        DoorDto doorDto = new DoorDto();
-
-        doorDto.setName("Side Door");
-        assertEquals("Side Door", doorDto.getName());
-    }*/
 }
